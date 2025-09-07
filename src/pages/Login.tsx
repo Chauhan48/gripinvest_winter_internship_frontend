a9605f261c74e6913a5750f3cbb1aeae328678e3
Anshit Chauhan <chauhananshit48@gmail.com>
2025-09-07 11:27:37 +0530

    feat: handle signin and register logic

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,8 +3,11 @@ import { Container, Typography, Box, TextField, Button, Card, CardContent } from
 import { useNavigate } from 'react-router-dom';
 
 const Login: React.FC = () => {
+  const [first_name, setFirstName] = useState('');
+  const [last_name, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [signinToggle, setSigninToggle] = useState(true);
   const navigate = useNavigate();
 
   const handleLogin = (e: React.FormEvent) => {
@@ -12,6 +15,10 @@ const Login: React.FC = () => {
     localStorage.setItem('isAuthenticated', 'true');
     navigate('/dashboard');
   };
+
+  const handleSigninToglle = () => {
+    setSigninToggle(!signinToggle);
+  }
 
   return (
     <Container maxWidth="sm">
@@ -22,9 +29,33 @@ const Login: React.FC = () => {
         <Card sx={{ width: '100%', mt: 2 }}>
           <CardContent>
             <Typography variant="h5" component="h2" gutterBottom align="center">
-              Sign In
+              {signinToggle ? "Sign In" : "Register"}
             </Typography>
             <Box component="form" onSubmit={handleLogin} sx={{ mt: 2 }}>
+              {signinToggle ? (<><TextField
+                margin="normal"
+                required
+                fullWidth
+                id="first_name"
+                label="First Name"
+                name="first_name"
+                autoComplete="first_name"
+                autoFocus
+                value={first_name}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="last_name"
+                  label="Last Name"
+                  name="last_name"
+                  autoComplete="last_name"
+                  autoFocus
+                  value={last_name}
+                  onChange={(e) => setLastName(e.target.value)}
+                /></>) : ''}
               <TextField
                 margin="normal"
                 required
@@ -55,8 +86,11 @@ const Login: React.FC = () => {
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
               >
-                Sign In
+                {signinToggle ? "Register" : "Sign In"}
               </Button>
+              <p onClick={handleSigninToglle} style={{ cursor: 'pointer', color: '#1976d2', textDecoration: 'underline' }}>
+                {signinToggle ? "Already have an account? Sign In" : "New user? Register"}
+              </p>
             </Box>
           </CardContent>
         </Card>
